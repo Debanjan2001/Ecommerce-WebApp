36da681d60dc078ddceb27cbc0f77c7b477063e3
Debanjan2001 <debanjansaha2001@gmail.com>
2021-01-22 01:27:42 +0530

    Minor frontend Changes

--- shop/views.py
+++ shop/views.py
@@ -4,12 +4,12 @@ from django.shortcuts import redirect, render
 from django.urls.base import reverse_lazy
 from django.views.generic import TemplateView,CreateView,UpdateView,DeleteView,ListView,DetailView
 from . models import Product
+import re
 # Create your views here.
 
 class Homepage(ListView):
     model = Product
     template_name = 'shop/homepage.html'
-
 
 class detailpage(DetailView):
     model = Product
@@ -17,31 +17,30 @@ class detailpage(DetailView):
 class createproduct(CreateView):
     model = Product
     fields = '__all__'
-    success_url = reverse_lazy('shop:home') 
+    success_url = reverse_lazy('shop:homepage')
 
 class deleteproduct(DeleteView):
     model = Product
-    success_url = reverse_lazy('shop:home')
+    success_url = reverse_lazy('shop:homepage')
 
 class updateproduct(UpdateView):
     model = Product
     fields = '__all__'
-    success_url = reverse_lazy('shop:home')
+    success_url = reverse_lazy('shop:homepage')
     template_name = 'shop/product_update.html'
+
 
 def search_text(request):
     if request.method == 'POST':
         text = request.POST.get("search_text")
-        print(text)
-        print("Hel")
         result =[]
         
         for product in Product.objects.all():
-            if text == product.name[0]:
+            if re.search(text,product.name) == True:
                 result.append(product) 
-        return render(request,'shop/searchpage.html',{'result':result})
+        return render(request,'shop/custom_search.html',{'result':result})
     else:
-        return render(request,'shop/searchpage.html')
+        return render(request,'shop/custom_search.html')
 
 def cart(request):
     return render(request, 'shop/cart.html')
